e9545485d75ab6219681bab644e2bf0f2958ce47
yasasvi <yasasvigumma@gmail.com>
2023-10-20 21:39:36 +0530

    added homepage with route

---
+++ src/components/HomePage.tsx
@@ -0,0 +1,13 @@
+import 'react'
+
+const HomePage = () => {
+  return (
+    <div className='bg-fakezon-background'>
+      <div className='min-w-[1000px] max-w-[1500px] m-auto bg-purple-700'>
+        Testing ....
+      </div>
+    </div>
+  )
+}
+
+export default HomePage
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        fakezon:{
+          background:"#EAEDED",
+          light_blue:"#232F3A",
+          yellow:"#FEBD69",
+          DEFAULT:"#131921"
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
